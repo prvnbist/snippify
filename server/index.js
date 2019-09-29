c6cc70846e4c3b68ff92fa7ef2111081deb8603d
Praveen Bisht <prvnbist@gmail.com>
2019-09-30 01:28:32 +0530

    Added: Get file handler

--- server/index.js
+++ server/index.js
@@ -1,56 +1,65 @@
-const fileUpload = require("express-fileupload");
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const app = express();
+const fileUpload = require('express-fileupload')
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const app = express()
 
-const { isFileAttached } = require("./utils/middlewares");
-const { getAllFiles } = require("./utils/files.js");
+const { isFileAttached } = require('./utils/middlewares')
+const { getAllFiles } = require('./utils/files.js')
 
 // default options
-app.use(fileUpload());
+app.use(fileUpload())
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, '..', '/client/build')))
 
-const defaultLocation = "../";
+const defaultLocation = '..'
 
 // Get list of all files
-app.get("/", (req, res) => {
-  return getAllFiles(`${defaultLocation}/snippets`).then(files =>
-    res.send({ files })
-  );
-});
+app.get('/files', (req, res) => {
+	return getAllFiles(`${defaultLocation}/snippets`).then(files =>
+		res.send({ files })
+	)
+})
+
+app.get('/file', (req, res) => {
+	const { type, name } = req.query
+	const filePath = `${defaultLocation}/snippets/${type}/${name}`
+	fs.readFile(filePath, (err, data) => {
+		if (err) return err
+		res.status(200).send({ file: data.toString() })
+	})
+})
 
 // Create, Update Files
-app.post("/", isFileAttached, (req, res) => {
-  const { file } = req.files;
-  const saveLocation = `${defaultLocation}/snippets/${
-    file.mimetype.split("/")[1]
-  }`;
-  const savePath = `${saveLocation}/${file.name}`;
-  if (!fs.existsSync(saveLocation)) {
-    fs.mkdirSync(saveLocation, { recursive: true });
-  }
-  fs.writeFile(savePath, file.data.toString(), err => {
-    if (err) throw err;
-    res.status(200).send({ message: "The file has been saved!" });
-  });
-});
+app.post('/createFile', isFileAttached, (req, res) => {
+	const { file } = req.files
+	const saveLocation = `${defaultLocation}/snippets/${
+		file.mimetype.split('/')[1]
+	}`
+	const savePath = `${saveLocation}/${file.name}`
+	if (!fs.existsSync(saveLocation)) {
+		fs.mkdirSync(saveLocation, { recursive: true })
+	}
+	fs.writeFile(savePath, file.data.toString(), err => {
+		if (err) throw err
+		res.status(200).send({ message: 'The file has been saved!' })
+	})
+})
 
 // TODO: Delete folder if empty after deleting files
 // Delete Files
-app.delete("/", (req, res) => {
-  const filePath = `${defaultLocation}/snippets/${req.body.type}/${req.body.fileName}`;
-  fs.unlink(filePath, err => {
-    if (err) throw err;
-    res.status(200).send({ message: "File has been deleted" });
-  });
-});
+app.delete('/deleteFile', (req, res) => {
+	const filePath = `${defaultLocation}/snippets/${req.body.type}/${req.body.fileName}`
+	fs.unlink(filePath, err => {
+		if (err) throw err
+		res.status(200).send({ message: 'File has been deleted' })
+	})
+})
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "../client/build/index.html"));
-});
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '..', '/client/build/index.html'))
+})
 
-const PORT = 4000;
+const PORT = 4000
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
